73ccf2d6f8bbfa85fa573d01dfd1eee9efaf1e81
Moenupa <mineworldy@outlook.com>
2020-05-30 15:26:13 +0800

    📙 GridSearchCV update: CV for XGBoost

--- titanic/gridsearch.py
+++ titanic/gridsearch.py
@@ -109,64 +109,18 @@ def main():
     # saving processed X_train data
     X_train.to_csv("C:\\Github\\MINEWORLDY\\titanic\\X_train_proc.csv", index=False)
     X_test.to_csv("C:\\Github\\MINEWORLDY\\titanic\\X_test_proc.csv", index=False)
-    models = ["RF"]
-    # models = ["XG","ADA","BG","ET","RF","ST","KN","DT", "SVC", "LSVC", "NSVC", "ST2", "MLP", "GB"]
-    for model in models:
-        classifier = train_model(model, X_train.drop(columns=['PassengerId']), y_train)
-        y_pred = classifier.predict(X_test.drop(columns=['PassengerId']))
-        result = pd.concat([X_test, pd.DataFrame(y_pred, columns=["Survived"])], axis=1)
-        print("\t{0}\t{1}".format(model, sum(y_pred)/152-1))
-        result.to_csv("C:\\Github\\MINEWORLDY\\titanic\\output\\data\\{0}_data.csv".format(model), index=False)
-        result.loc[:,['PassengerId','Survived']].to_csv("C:\\Github\\MINEWORLDY\\titanic\\output\\{0}_output.csv".format(model), index=False)
-def train_model(model, X_train, y_train):
-    '''
-    Process one model by training data
-    Input: X_train,y_train
-    Output: regressor by the need
-    '''
-    if model == 'XG':
-        clsfr = XGBClassifier()
-    elif model == 'ADA':
-        clsfr = AdaBoostClassifier()
-    elif model == 'DT':
-        clsfr = DecisionTreeClassifier()
-    elif model == 'SVC':
-        clsfr = SVC()
-    elif model == 'KN':
-        clsfr = KNeighborsClassifier(n_neighbors=5, weights="uniform", algorithm="auto",
-            leaf_size=30, p=2, metric="minkowski", metric_params=None)
-    elif model == 'BG':
-        clsfr = BaggingClassifier(base_estimator=RandomForestClassifier())
-    elif model == 'ET':
-        clsfr = ExtraTreesClassifier()
-    elif model == 'RF':
-        clsfr = RandomForestClassifier()
-    elif model == 'ST':
-        estimators = [
-            ('MLP',MLPClassifier()),
-            ('RF',RandomForestClassifier()),
-            ('XG',XGBClassifier()),
-            ('ADA',AdaBoostClassifier())
-        ]
-        clsfr = StackingClassifier(estimators=estimators)
-    elif model == 'NSVC':
-        clsfr = NuSVC()
-    elif model == 'LSVC':
-        clsfr = LinearSVC()
-    elif model == 'ST2':
-        estimators = [
-            XGBClassifier(),
-            AdaBoostClassifier(),
-            RandomForestClassifier(),
-            MLPClassifier()]
-        clsfr = StackingCVClassifier(classifiers = estimators, meta_classifier = MLPClassifier())
-    elif model == 'MLP':
-        clsfr = MLPClassifier(learning_rate='adaptive', max_iter=1000)
-    elif model == 'GB':
-        clsfr = GradientBoostingClassifier()
-
-    clsfr.fit(X_train,y_train)
-    return clsfr
+    param_dict = {
+        'n_estimators':range(80,200,4),
+        'max_depth':range(2,15,1),
+        'learning_rate':np.linspace(0.01,2,20),
+        'subsample':np.linspace(0.7,0.9,20),
+        'colsample_bytree':np.linspace(0.5,0.98,10),
+        'min_child_weight':range(1,9,1)
+    }
+    classifier = XGBClassifier()
+    grid = GridSearchCV(classifier, param_dict, cv = 3, scoring = 'roc_auc', n_jobs = -1)
+    grid.fit(X_train, y_train)
+    print(grid.best_estimator)
 
 if __name__ == "__main__":
     main()
